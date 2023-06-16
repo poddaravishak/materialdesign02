78b888e4179c49833f2cd7aaf7d048fa8283c676
avishak-poddar <avishakpoddar@gmail.com>
2023-06-16 16:18:07 +0600

    Home page design start

--- app/src/main/java/com/eterces/material_design02/MainActivity.java
+++ app/src/main/java/com/eterces/material_design02/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_page);
+        setContentView(R.layout.mainpage);
     }
 }
